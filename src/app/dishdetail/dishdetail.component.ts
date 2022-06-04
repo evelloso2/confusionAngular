2ef9e2751c631fd4986d65d541ac669982f56188
evelloso2 <euclides.velloso@tjdft.jus.br>
2022-06-04 13:57:51 -0300

    Single Page Applications - SPA - Router Parameters

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
 
+import { DishService } from '../services/dish.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 
   
 @Component({
@@ -8,15 +13,22 @@ import { Dish } from '../shared/dish';
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
-export class DishdetailComponent implements OnInit {  
 
-    
-  @Input()
+export class DishdetailComponent implements OnInit {
+
   dish!: Dish;
 
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id.toString());
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
